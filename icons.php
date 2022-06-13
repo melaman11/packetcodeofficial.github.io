4dbfbb1588b045b1378802eac1f7a9f75a633ec7
melaman11 <104849211+melaman11@users.noreply.github.com>
2022-06-13 13:42:04 -0700

    Revert "Added new files"
    This reverts commit c5f55c8636a8ea7b667e9542ff4c6bdc13de8682.

--- icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "functions/functions.php";
-
-$oParts = new Parts();
-$oParts->addHeader();
-$oParts->addIcons();
-$oParts->addFooter2();
--- order1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "functions/functions.php";
-
-$oParts = new Parts();
-$oParts->addHeader();
-$oParts->addOrder();
-$oParts->addFooter2();
